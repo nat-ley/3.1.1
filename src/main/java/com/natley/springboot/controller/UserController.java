17ab3cf2d99af6f18e43efb249e84c69dc78c8f2
nat-ley <nat_ley@mail.ru>
2022-03-06 22:49:05 +0400

    3.1.3

--- src/main/java/com/natley/springboot/controller/UserController.java
+++ src/main/java/com/natley/springboot/controller/UserController.java
@@ -25,24 +25,11 @@ public class UserController {
 
 
 	@RequestMapping("/admin")
-	public String admin(Model model) {
+	public String admin(Principal principal, Model model) {
 		model.addAttribute("users_list", userService.getAllUsers());
-		return "admin";
-	}
-
-	@RequestMapping("/new")
-	public String newUserForm(Model model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		return "new_user";
-	}
-
-	@RequestMapping("/edit/{id}")
-	public String editUserForm(@PathVariable long id, Model model) {
-		User user = userService.readUser(id);
-		model.addAttribute("user", user);
+		model.addAttribute("current_user", userService.findByEmail(principal.getName()));
 		model.addAttribute("allRoles", roleService.getAll());
-		return "edit_users";
+		return "admin";
 	}
 
 	@RequestMapping("/save")
@@ -58,8 +45,7 @@ public class UserController {
 	}
 	@RequestMapping("/user")
 	public String users(Principal principal, Model model) {
-		model.addAttribute("user", userService.findByEmail(principal.getName()));
+		model.addAttribute("current_user", userService.findByEmail(principal.getName()));
 		return "user";
 	}
-
 }
--- src/main/java/com/natley/springboot/model/Role.java
+++ src/main/java/com/natley/springboot/model/Role.java
@@ -34,8 +34,6 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "name='" + name + '\'' +
-                '}';
+        return name;
     }
 }
